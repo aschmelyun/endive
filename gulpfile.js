371ef4991a0e3a29647eed39158ca5fb78d70f11
Andrew Schmelyun <schmelyun@gmail.com>
2016-06-28 13:16:37 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+var sass     = require('gulp-sass'),
+    concat   = require('gulp-concat'),
+    uglify   = require('gulp-uglify'),
+    cleancss = require('gulp-clean-css'),
+    rename   = require('gulp-rename');
+
+gulp.task('sass', function() {
+    return gulp.src('src/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('build'))
+        .pipe(concat('endive.css'))
+        .pipe(gulp.dest('build'))
+        .pipe(cleancss())
+        .pipe(rename('endive.min.css'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['sass']);
